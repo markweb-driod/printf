ce789a8976d2c72852ff9bcdf2feb932d5694ea2
Kester George <kestercode@gmail.com>
2023-07-24 11:46:31 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,26 +1,28 @@
-/** jkprintf - Custom printf function working according to ALX specification
- *
- */
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+
+/**
+ * jkprintf - Custom printf function working according to ALX specification
+ * @ratval : the charter counter to be returned
+ */
+
 int _printf(const char *format, ...)
 {
-        int ratval= 0;
-        int count=0;
+	int ratval = 0;
+	int count = 0;
 
-        va_list args;
-        va_start(args, format);
+	va_list args;
+	va_start(args, format);
 
-        while (format[count] != '\0')
-        {
-                if (format[count] != '%')
-                {
-                        jkputchar(format[count]);
-                        ratval = ratval + 1;
-                }
-                count++;
-
-        }
-        return (ratval);
+	while (format[count] != '\0')
+	{
+		if (format[count] != '%')
+		{
+			jkputchar(format[count]);
+			ratval = ratval + 1;
+		}
+		count++;
+	}
+	return (ratval);
 }
--- jkputchar.c
+++ jkputchar.c
@@ -1,10 +1,13 @@
-/** custom Putchar file
- * @return: return 
- *
+/**
+ * jkputchar - writes the character c to stdout
+ * @a: The character to printto stndard output
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+
 #include "main.h"
 
 int jkputchar(char a)
 {
-        return(write(1, &a, 1));
+	return (write(1, &a, 1));
 }
