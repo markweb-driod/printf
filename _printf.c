27916ae707734942f08d7fa6510c60eeafe7dba7
Kester George <kestercode@gmail.com>
2023-07-25 04:14:10 -0700

    updating

--- _printf.c
+++ _printf.c
@@ -12,16 +12,21 @@ int _printf(const char *format, ...)
 	int ratval = 0;
 	int count = 0;
 
-	va_list args
-		va_start(args, format);
+	va_list args;
+	va_start(args, format);
 
 	while (format[count] != '\0')
 	{
 		if (format[count] != '%')
 		{
 			jkputchar(format [count]);
-			ratval- ratval + 1;
 		}
+		else if (format[count + 1] == 'c')
+                {
+			jkputchar(va_arg(args, int));
+			count++;
+                }
+		ratval = ratval + 1;
 		count++;
 	}
 	return (ratval);
