d6118bcb5156d66899929928a130cbb70774522a
Kester George <kestercode@gmail.com>
2023-07-25 06:25:07 -0700

    Updating

--- _printf.c
+++ _printf.c
@@ -3,8 +3,9 @@
 #include <stdarg.h>
 
 /**
- * jkprintf - custon printf function working according to ALX specification 
- * @ratval : the character counter to be returned
+ * _printf - custon printf function working according to ALX specificatio
+ * Return : the character counter to be returned
+ * @*format: format spicifier of the print parameter
  */
 
 int _printf(const char *format, ...)
@@ -13,21 +14,25 @@ int _printf(const char *format, ...)
 	int count = 0;
 
 	va_list args;
+
 	va_start(args, format);
 
 	while (format[count] != '\0')
 	{
 		if (format[count] != '%')
 		{
-			jkputchar(format [count]);
+			jkputchar(format[count]);
 		}
 		else if (format[count + 1] == 'c')
-                {
+		{
 			jkputchar(va_arg(args, int));
 			count++;
-                }
-		else if(format[count + 1] == '%')
+		}
+		else if (format[count + 1] == '%')
+		{
 			jkputchar('%');
+			count++
+		}
 		ratval = ratval + 1;
 		count++;
 	}
