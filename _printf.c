06379f68cd223cb3c5ce1ed15e7a579d9cd3856c
Kester George <kestercode@gmail.com>
2023-07-25 11:20:11 -0700

    corrections

--- _printf.c
+++ _printf.c
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 		if (format[count] != '%')
 		{
 			jkputchar(format[count]);
-			ratval - ratval + 1;
+			ratval = ratval + 1;
 		}
 		else if (format[count + 1] == 'c')
 		{
